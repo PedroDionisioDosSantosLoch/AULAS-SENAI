670768d1a9d420bd869c7d548b4cf16e8168667c
PedroDionisioDosSantosLoch <pedrodionisiosantosloch@gmail.com>
2025-06-18 10:47:50 -0300

    Update 12,06.js

--- 12,06.js
+++ 12,06.js
@@ -6,12 +6,12 @@ let indicePerguntas = 0
 
 // Arrays
 const perguntas = [
-    {pergunta:"Qual é o maior deserto do mundo?",opcao:["Atacama", "Antartica" , "Saara"]},
-    {pergunta:"Qual é a capital do Brasil?",opcao:["Acre", "Amazonas" , "brasilia"]},
-     {pergunta:"Qual é a capital da Austrália?",opcao:["Berlim", "Nove York" , "Camberra"]},
-     {pergunta:"Qual é o país com maior população no mundo?",opcao:["Monaco", "China" , "India"]},
-     {pergunta:" Qual a linha imaginária que atravessa o Brasil?",opcao:["Feilicidade", "Japão" , "Equador"]},
-     {pergunta:"Qual o oceano que banha o Brasil?",opcao:["Azul", "Vermelho" , "Atlântico"]},
+    {pergunta:"Qual é o maior deserto do mundo?",opcoes:["Atacama", "Antartica" , "Saara"]},
+    {pergunta:"Qual é a capital do Brasil?",opcoes:["Acre", "Amazonas" , "brasilia"]},
+     {pergunta:"Qual é a capital da Austrália?",opcoes:["Berlim", "Nove York" , "Camberra"]},
+     {pergunta:"Qual é o país com maior população no mundo?",opcoes:["Monaco", "China" , "India"]},
+     {pergunta:" Qual a linha imaginária que atravessa o Brasil?",opcoes:["Feilicidade", "Japão" , "Equador"]},
+     {pergunta:"Qual o oceano que banha o Brasil?",opcoes:["Azul", "Vermelho" , "Atlântico"]},
 ]
 
 
@@ -30,22 +30,28 @@ function fecharbotaoinicial(){
 
 function abrirteladojogo(){
     divcaixaQuiz.classList.add("active");
-    h2perguntadoquiz.textContent = perguntas [indicePerguntas].pergunta
+
+    h2perguntadoquiz.textContent = perguntas [indicePerguntas].pergunta;
     //adicionando um botao ao painel
-    const botaopergunta = document.createElement("button");
+    perguntas[indicePerguntas].opcoes.forEach(opcao => {
     
+    const botaopergunta = document.createElement("button");
     //adicionando texto no botao
-    botaopergunta.textContent = perguntas [indicePerguntas].pergunta;
+    botaopergunta.textContent = opcao
     
     //adicionar uma classe css no botao
     botaopergunta.classList.add("answer-btn");
     opcoesRespostas.appendChild(botaopergunta);
+})
 }
 
 function proximapergunta(){
         indicePerguntas++
         if(indicePerguntas < perguntas.length){
             abrirteladojogo()
+    }
+
+}
     }
 
 }
