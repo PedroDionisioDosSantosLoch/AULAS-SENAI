41d9c767dc199b0c192e2232dce2757ab3a3be3a
PedroDionisioDosSantosLoch <pedrodionisiosantosloch@gmail.com>
2025-06-18 10:17:27 -0300

    Update 12,06.js

--- 12,06.js
+++ 12,06.js
@@ -1,17 +1,17 @@
 const divbotaoiniciar = document.getElementById("IniciarJogo")
 const divcaixaQuiz = document.getElementById("caixaQuiz")
-
+const h2perguntadoquiz = document.getElementById("perguntadoquiz")
 
 let indicePerguntas = 0
 
 // Arrays
 const perguntas = [
-    "Qual é o maior deserto do mundo?",
-     "Qual é a capital do Brasil?",
-     "Qual é a capital da Austrália?",
-     "Qual é o país com maior população no mundo?",
-     " Qual a linha imaginária que atravessa o Brasil?",
-     "Qual o oceano que banha o Brasil?",
+    {pergunta:"Qual é o maior deserto do mundo?",opcao:["Atacama", "Antartica" , "Saara"]},
+    {pergunta:"Qual é a capital do Brasil?",opcao:["Acre", "Amazonas" , "brasilia"]},
+     {pergunta:"Qual é a capital da Austrália?",opcao:["Berlim", "Nove York" , "Camberra"]},
+     {pergunta:"Qual é o país com maior população no mundo?",opcao:["Monaco", "China" , "India"]},
+     {pergunta:" Qual a linha imaginária que atravessa o Brasil?",opcao:["Feilicidade", "Japão" , "Equador"]},
+     {pergunta:"Qual o oceano que banha o Brasil?",opcao:["Azul", "Vermelho" , "Atlântico"]},
 ]
 
 
@@ -30,12 +30,12 @@ function fecharbotaoinicial(){
 
 function abrirteladojogo(){
     divcaixaQuiz.classList.add("active");
-    
+    h2perguntadoquiz.textContent = perguntas [indicePerguntas].pergunta
     //adicionando um botao ao painel
     const botaopergunta = document.createElement("button");
     
     //adicionando texto no botao
-    botaopergunta.textContent = perguntas [indicePerguntas];
+    botaopergunta.textContent = perguntas [indicePerguntas].pergunta;
     
     //adicionar uma classe css no botao
     botaopergunta.classList.add("answer-btn");
